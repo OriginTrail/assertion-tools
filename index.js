a639079c24721abade1b6e99a17fc82abc30335c
zeroxbt <original.swazi@protonmail.com>
2022-08-03 01:22:07 +0200

    remove reduntant promise

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-const jsonld = require('jsonld');
-const keccak256 = require('keccak256')
-const web3 = require('web3')
-const {MerkleTree} = require('merkletreejs')
-const {sha256} = require('multiformats/hashes/sha2');
+const jsonld = require("jsonld");
+const keccak256 = require("keccak256");
+const web3 = require("web3");
+const { MerkleTree } = require("merkletreejs");
+const { sha256 } = require("multiformats/hashes/sha2");
 
-function formatAssertion(json) {
-    return new Promise(async (accept, reject) => {
-        const compactedJson = await jsonld.compact(json, {
-            "@context": "https://schema.org/",
-        });
+async function formatAssertion(json) {
+  const compactedJson = await jsonld.compact(json, {
+    "@context": "https://schema.org/",
+  });
 
-        const canonizedJson = await jsonld.canonize(compactedJson, {
-            algorithm: "URDNA2015",
-            format: "application/n-quads",
-        });
+  const canonizedJson = await jsonld.canonize(compactedJson, {
+    algorithm: "URDNA2015",
+    format: "application/n-quads",
+  });
 
-        const assertion = canonizedJson.split('\n').filter((x) => x !== '');
+  const assertion = canonizedJson.split("\n").filter((x) => x !== "");
 
-        if (assertion && assertion.length === 0) {
-            reject('File format is corrupted, no n-quads are extracted.');
-        }
+  if (assertion && assertion.length === 0) {
+    reject("File format is corrupted, no n-quads are extracted.");
+  }
 
-        accept(assertion);
-    });
+  return assertion;
 }
 
 function calculateRoot(assertion) {
-    assertion.sort();
-    const leaves = assertion.map((element, index) => keccak256(web3.utils.encodePacked(
-        keccak256(element),
-        index
-    )))
-    const tree = new MerkleTree(leaves, keccak256, {sortPairs: true})
-    return `0x${tree.getRoot().toString('hex')}`;
+  assertion.sort();
+  const leaves = assertion.map((element, index) =>
+    keccak256(web3.utils.encodePacked(keccak256(element), index))
+  );
+  const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+  return `0x${tree.getRoot().toString("hex")}`;
 }
 
 async function peerId2Hash(peerId) {
-    return `0x${Buffer.from((await sha256.digest(peerId.toBytes())).digest).toString('hex')}`;
+  return `0x${Buffer.from(
+    (await sha256.digest(peerId.toBytes())).digest
+  ).toString("hex")}`;
 }
 
 module.exports = {
-    formatAssertion,
-    calculateRoot,
-    peerId2Hash,
-}
+  formatAssertion,
+  calculateRoot,
+  peerId2Hash,
+};
