ee022636ceb98672da075892df67e1d62ac7e2ca
Uladzislau Hubar <hubar.uladzislau@gmail.com>
2022-10-27 11:46:52 +0200

    Removed peerId2Hash as it's not used anymore + dependencies

--- peer-id-2-hash.js
+++
@@ -1,7 +0,0 @@
-const { sha256 } = require("multiformats/hashes/sha2");
-
-module.exports = peerId2Hash = async (peerId) => {
-  return `0x${Buffer.from(
-    (await sha256.digest(peerId.toBytes())).digest
-  ).toString("hex")}`;
-};
