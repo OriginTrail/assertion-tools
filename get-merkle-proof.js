673694664e279de03ff2df590b3d7a9624adabf7
Uladzislau Hubar <hubar.uladzislau@gmail.com>
2022-11-22 03:04:15 +0100

    Added ethers.js instead of web3.js, fixed Merkle proof calculation bug

--- get-merkle-proof.js
+++ get-merkle-proof.js
@@ -1,17 +1,22 @@
-const keccak256 = require("keccak256");
-const web3 = require("web3");
+const ethers = require("ethers");
+const keccak256 = require("./keccak256.js")
 const { MerkleTree } = require("merkletreejs");
 
 module.exports = getMerkleProof = (nquadsArray, challenge) => {
   nquadsArray.sort();
 
   const leaves = nquadsArray.map((element, index) =>
-    keccak256(web3.utils.encodePacked(keccak256(element), index))
+    keccak256(
+      ethers.utils.solidityPack(
+        ["bytes32", "uint256"],
+        [keccak256(element), index],
+      )
+    )
   );
   const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
 
   return {
-    leaf: keccak256[nquadsArray(challenge)],
+    leaf: keccak256(nquadsArray[challenge]),
     proof: tree.getHexProof(leaves[challenge]),
   };
 };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const calculateRoot = require("./calculate-root.js");
 const formatAssertion = require("./format-assertion.js");
 const peerId2Hash = require("./peer-id-2-hash.js");
-const getMerkleProof = require("./get-merkle-proof.js")
+const getMerkleProof = require("./get-merkle-proof.js");
+const keccak256 = require("./keccak256.js");
 
-module.exports = { calculateRoot, formatAssertion, peerId2Hash, getMerkleProof };
+module.exports = { calculateRoot, formatAssertion, peerId2Hash, getMerkleProof, keccak256 };
