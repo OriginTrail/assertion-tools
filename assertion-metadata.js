bbf70bdf8079b738d54bc93a4d0497867a59f0dc
zeroxbt <original.swazi@protonmail.com>
2022-11-24 17:07:04 +0100

    fix exports, bump version

--- assertion-metadata.js
+++ assertion-metadata.js
@@ -1,11 +1,11 @@
-module.exports = getAssertionSizeInBytes = (assertion) => {
+module.exports.getAssertionSizeInBytes = (assertion) => {
     return Buffer.byteLength(JSON.stringify(assertion));
 };
 
-module.exports = getAssertionTriplesNumber = (assertion) => {
+module.exports.getAssertionTriplesNumber = (assertion) => {
     return assertion.length;
 };
 
-module.exports = getAssertionChunksNumber = (assertion) => {
+module.exports.getAssertionChunksNumber = (assertion) => {
     return assertion.length;
 };
