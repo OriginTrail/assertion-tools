3a262415b7e0da1c56157b3a2131a35d0be4ebff
brkagithub <mbrkic16@gmail.com>
2024-11-22 11:46:30 +0100

    Export v8 functions

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ const {
   keccak256
 } = require('./knowledge-asset-tools.js');
 
-module.exports = { calculateRoot, formatAssertion, formatGraph, getMerkleProof, peerId2Hash, keccak256, assertionMetadata };
+const {
+  calculateNumberOfChunks,
+  calculateByteSize,
+  flattenDataset,
+  formatDataset
+} = require('./knowledge-collection-tools.js');
+
+module.exports = { calculateRoot, formatAssertion, formatGraph, getMerkleProof, peerId2Hash, keccak256, assertionMetadata, calculateNumberOfChunks, calculateByteSize, flattenDataset,
+  formatDataset };
